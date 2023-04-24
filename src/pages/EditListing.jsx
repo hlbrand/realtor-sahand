f7c92e44987e98d041a428ede9803439331c726d
hlbrand <hlbrand2995@gmail.com>
2023-04-25 06:18:22 +0700

    add the Listing information to the Listing page

--- src/pages/EditListing.jsx
+++ src/pages/EditListing.jsx
@@ -480,7 +480,6 @@ export default function EditListing() {
             onChange={onChange}
             accept=".jpg,.png,.jpeg"
             multiple
-            required
             className="w-full px-3 py-1.5 text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:bg-white focus:border-slate-600"
           />
         </div>
